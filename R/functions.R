e78d8405ba5a19518ca1eeb1d574868d8d260318
Zezhen Wu <wuzezhen33@gmail.com>
2023-12-01 16:16:16 -0500

    EDA

--- R/functions.R
+++ R/functions.R
@@ -23,3 +23,39 @@ get_rel <- function(model, data){
     "McDonald's Omega is ", round(omega, 3), ", which is ", omega_interpretation, "."
   ))
 }
+
+# get factor score from lavaan model and merge it to original data
+get_fscore <- function(fit, varname, data){
+  
+  pred <- lavaan::lavPredict(fit)
+  index <- lavaan::inspect(fit, "case.idx")
+  
+  data[index, varname] <- pred[,1]
+  
+  return(data)
+}
+
+# function to get correlation
+# correlation matrix with significance stars
+corstarsl <- function(x){ 
+  x <- as.matrix(x) 
+  R <- Hmisc::rcorr(x)$r 
+  p <- Hmisc::rcorr(x)$P 
+  ## define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .001, "***", ifelse(p < .01, "**", ifelse(p < .05, "*", "")))
+  ## trunctuate the matrix that holds the correlations to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 3))[,-1] 
+  ## build a new matrix that includes the correlations with their apropriate stars 
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x)) 
+  diag(Rnew) <- paste(diag(R), " ", sep="") 
+  rownames(Rnew) <- colnames(x) 
+  colnames(Rnew) <- paste(colnames(x), "", sep="") 
+  ## remove upper triangle
+  Rnew <- as.matrix(Rnew)
+  Rnew[upper.tri(Rnew, diag = TRUE)] <- "--"
+  Rnew[upper.tri(Rnew)] <- ""
+  Rnew <- as.data.frame(Rnew) 
+  ## remove last column and return the matrix (which is now a data frame)
+  Rnew <- cbind(Rnew[1:length(Rnew)])
+  return(Rnew) 
+}
